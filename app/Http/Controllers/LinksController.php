298331aeb37a7d12965ea32b4bd91f84aa237e73
César Ramírez <cesar.ramirez.sv@gmail.com>
2017-05-20 12:00:40 -0600

    Requesting short link details

--- app/Http/Controllers/LinksController.php
+++ app/Http/Controllers/LinksController.php
@@ -40,6 +40,19 @@ class LinksController extends Controller
         return $this->linkResponse($link);
     }
 
+    public function show(Request $request)
+    {
+        $code = $request->get('code');
 
+        $link = $this->link->byCode($code)->first();
+
+        if ( $link === null) {
+            return response(null, 404);
+        }
+
+        $link->increment('used_count');
+        
+        return $this->linkResponse($link);
+    }
 
 }
